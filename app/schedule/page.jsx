75b7f9ed922e559b952e59d8fcfe0ef02f3ae62e
Clarel Antoine <clarel.antoine93@gmail.com>
2024-02-02 05:32:03 +0400

    setup schedule filter

--- app/schedule/page.jsx
+++ app/schedule/page.jsx
@@ -15,11 +15,8 @@ import { ScheduleContext } from "@/contexts/schedule.context";
 export default function Schedule () {
     const {schedules} = useContext(ScheduleContext)
 
-    const {meetings, setMeetings} = useState(null)
+    const [meetings, setMeetings] = useState(null)
     const [day, setDay] = useState(1)
-
-
-
 
     const handleDayChange = (event) => {
 
@@ -32,24 +29,47 @@ export default function Schedule () {
         })
 
         event.target.classList.add('active')
-        console.log(dataset.day);
+        // console.log(dataset.day);
 
-        setDay[dataset.day]
+        setDay(parseInt(dataset.day))
     }
 
     const getSelectedMeetingsDay = (day) => {
-        // const selectedMeetingsDay = schedules.filter(item => {
-        //     if(item.Schedule)
-        // })
+        const selectedMeetingsDay = schedules.map(item => {
+            const splitDate = item.Schedule.split(" ")
+            
 
-        return schedules.map(item => {
-            return item
+            const newDate = splitDate[0]
+            const StartTime = splitDate[1]
+
+            const splitNewTime = StartTime.split(":")
+            let now = new Date();
+            const EndTime = now.setHours(splitNewTime[0], splitNewTime[1] + item.Durationm, 0, 0)
+
+            const newTime = `${StartTime} - ${EndTime}`
+
+
+
+            return {...item, Schedule: newDate, Time: StartTime}
+        }).filter(item => {
+            return parseInt(item.Day) === day
         })
+
+        setMeetings(selectedMeetingsDay)
     }
 
     useEffect(() => {
-        console.log(getSelectedMeetingsDay())
+        // console.log("day changed");
+        getSelectedMeetingsDay(day)
+
     }, [day])
+    
+    useEffect(() => {
+        // console.log("day changed");
+        console.log(meetings);
+
+    }, [meetings])
+    
 
     return (
         <div className="schedule__wrapper">
@@ -66,48 +86,31 @@ export default function Schedule () {
                     <span className="room__navigation__arrow room__next"><ArrowForwardIosIcon /></span>
                 </div>
                 <div className="meetings">
-                    <div className="meeting__card">
-                        <span className="meeting__date meeting__detail">
-                            <CalendarMonthIcon />
-                            20/02/2024
-                        </span>
-                        <span className="meeting__time meeting__detail">
-                            <AccessTimeIcon />
-                            14:30 - 15:00
-                        </span>
-                        <span className="meeting__name meeting__detail">
-                            <PermIdentityIcon />
-                            John Doe
-                        </span>
-                    </div>
-                    <div className="meeting__card">
-                        <span className="meeting__date meeting__detail">
-                            <CalendarMonthIcon />
-                            20/02/2024
-                        </span>
-                        <span className="meeting__time meeting__detail">
-                            <AccessTimeIcon />
-                            14:30 - 15:00
-                        </span>
-                        <span className="meeting__name meeting__detail">
-                            <PermIdentityIcon />
-                            John Doe
-                        </span>
-                    </div>
-                    <div className="meeting__card">
-                        <span className="meeting__date meeting__detail">
-                            <CalendarMonthIcon />
-                            20/02/2024
-                        </span>
-                        <span className="meeting__time meeting__detail">
-                            <AccessTimeIcon />
-                            14:30 - 15:00
-                        </span>
-                        <span className="meeting__name meeting__detail">
-                            <PermIdentityIcon />
-                            John Doe
-                        </span>
-                    </div>
+                    
+                    {meetings && meetings.length > 0 ? (
+                        meetings.map((item, index) => (
+                            <div key={index} className="meeting__card">
+                                <span className="meeting__date meeting__detail">
+                                    <CalendarMonthIcon />
+                                    {/* 20/02/2024 */}
+                                    {item.Schedule}
+                                </span>
+                                <span className="meeting__time meeting__detail">
+                                    <AccessTimeIcon />
+                                    {/* 14:30 - 15:00 */}
+                                    {item.Time}
+                                </span>
+                                <span className="meeting__name meeting__detail">
+                                    <PermIdentityIcon />
+                                    {/* John Doe */}
+                                    {item.Company}
+                                </span>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="noschedule__message">No schedule yet...</p>
+                    )}
+                    
                 </div>
             </main>
             {/* <a href="/schedule/test">text link 1</a> */}
